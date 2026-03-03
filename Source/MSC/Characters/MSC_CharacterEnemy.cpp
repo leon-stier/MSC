091999c408a1e698f0294e828b29844c032db28b
Leon Stier <Lms@gnadenweiler.de>
2026-03-03 20:49:42 +0100

    Starting on Enemy

---
+++ Source/MSC/Characters/MSC_CharacterEnemy.cpp
@@ -0,0 +1,1 @@
+﻿#include "MSC_CharacterEnemy.h"
---
+++ Source/MSC/Characters/MSC_CharacterEnemy.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "MSC_CharacterBase.h"
+#include "MSC_CharacterEnemy.generated.h"
+
+UCLASS(abstract)
+class MSC_API AMSC_CharacterEnemy : public AMSC_CharacterBase
+{
+	GENERATED_BODY()
+public:
+	AMSC_CharacterEnemy();
+	
+};
