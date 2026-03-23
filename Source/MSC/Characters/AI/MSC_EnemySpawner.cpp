ff9a2db322fae73d4d4ddbc88f0d03cea9857e05
Leon Stier <Lms@gnadenweiler.de>
2026-03-23 15:57:30 +0100

    MT-32 Working on enemy spawner

---
+++ Source/MSC/Characters/AI/MSC_EnemySpawner.cpp
@@ -0,0 +1,80 @@
+﻿#include "MSC_EnemySpawner.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
+#include "MSC_CharacterEnemy.h"
+#include "Components/ArrowComponent.h"
+#include "Components/CapsuleComponent.h"
+
+AMSC_EnemySpawner::AMSC_EnemySpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// create the root
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	// create the reference spawn capsule
+	SpawnCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Spawn Capsule"));
+	SpawnCapsule->SetupAttachment(RootComponent);
+
+	SpawnCapsule->SetRelativeLocation(FVector(0.0f, 0.0f, 90.0f));
+	SpawnCapsule->SetCapsuleSize(35.0f, 90.0f);
+	SpawnCapsule->SetCollisionProfileName(FName("NoCollision"));
+
+	SpawnDirection = CreateDefaultSubobject<UArrowComponent>(TEXT("Spawn Direction"));
+	SpawnDirection->SetupAttachment(RootComponent);
+}
+
+void AMSC_EnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (bShouldSpawnEnemiesImmediately)
+	{
+		GetWorld()->GetTimerManager().SetTimer(SpawnTimer, this, &AMSC_EnemySpawner::SpawnEnemy, InitialSpawnDelay);
+	}
+}
+
+void AMSC_EnemySpawner::EndPlay(EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	
+	GetWorld()->GetTimerManager().ClearTimer(SpawnTimer);
+}
+
+void AMSC_EnemySpawner::SpawnEnemy()
+{
+	// ensure the enemy class is valid
+	if (IsValid(EnemyClass))
+	{
+		// spawn the enemy at the reference capsule's transform
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+		AMSC_CharacterEnemy* SpawnedEnemy = GetWorld()->SpawnActor<AMSC_CharacterEnemy>(EnemyClass, SpawnCapsule->GetComponentTransform(), SpawnParams);
+
+		// was the enemy successfully created?
+		if (SpawnedEnemy && SpawnedEnemy->GetAbilitySystemComponent())
+		{
+			// Listen for the dead tag on each enemy
+			SpawnedEnemy->GetAbilitySystemComponent()->RegisterGameplayTagEvent(
+				FGameplayTag::RequestGameplayTag(FName("Combat.Dead")),
+				EGameplayTagEventType::NewOrRemoved)
+				.AddUObject(this, &AMSC_EnemySpawner::OnDeadTagChanged);
+		}
+	}
+}
+
+
+void AMSC_EnemySpawner::OnDeadTagChanged(const FGameplayTag CallbackTag, int32 NewCount)
+{
+	if (NewCount > 0)
+	{
+		CurrentlyAlive--;
+	
+		if (CurrentlyAlive <= 0)
+		{
+			GetWorld()->GetTimerManager().SetTimer(SpawnTimer, this, &AMSC_EnemySpawner::SpawnEnemy, RespawnDelay);
+		}
+	}
+}
---
+++ Source/MSC/Characters/AI/MSC_EnemySpawner.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "MSC_EnemySpawner.generated.h"
+
+struct FGameplayTag;
+class AMSC_CharacterEnemy;
+class UCapsuleComponent;
+class UArrowComponent;
+
+UCLASS(abstract)
+class MSC_API AMSC_EnemySpawner : public AActor
+{
+	
+	GENERATED_BODY()
+	
+	AMSC_EnemySpawner();
+	
+	/** Initialization */
+	virtual void BeginPlay() override;
+
+	/** Cleanup */
+	virtual void EndPlay(EEndPlayReason::Type EndPlayReason) override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	UCapsuleComponent* SpawnCapsule;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UArrowComponent* SpawnDirection;
+	
+protected:
+	/** Spawn an enemy and subscribe to its death event */
+	void SpawnEnemy();
+
+	void OnDeadTagChanged(const FGameplayTag CallbackTag, int32 NewCount);
+	
+	/** Type of enemy to spawn */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner")
+	TSubclassOf<AMSC_CharacterEnemy> EnemyClass;
+
+	/** If true, the first enemy will be spawned as soon as the game starts */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner")
+	bool bShouldSpawnEnemiesImmediately = true;
+
+	/** Time to wait before spawning the first enemy on game start */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner", meta = (ClampMin = 0, ClampMax = 10))
+	float InitialSpawnDelay = 5.0f;
+
+	/** Number of enemies to spawn */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner", meta = (ClampMin = 0, ClampMax = 100))
+	int32 SpawnCount = 1;
+	
+
+	/** Time to wait before spawning the next enemy after the current one dies */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner", meta = (ClampMin = 0, ClampMax = 10))
+	float RespawnDelay = 5.0f;
+
+	/** Time to wait after this spawner is depleted before activating the actor list */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Activation", meta = (ClampMin = 0, ClampMax = 10))
+	float ActivationDelay = 1.0f;
+	
+	/** Flag to ensure this is only activated once */
+	bool bHasBeenActivated = false;
+
+	/** Timer to spawn enemies after a delay */
+	FTimerHandle SpawnTimer;
+	
+	int32 CurrentlyAlive = 0;
+};
